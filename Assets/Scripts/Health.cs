644292c34588d7294c6e5d44fe1815c8efceb22e
DariusRupsys <darrup@ktu.lt>
2024-03-11 22:39:24 +0200

    health and enemy

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public int maxHealth = 10;
+    public int currentHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+    public void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+        if(currentHealth <= 0)
+        {
+            // mirtis
+        }
+    }
+}
